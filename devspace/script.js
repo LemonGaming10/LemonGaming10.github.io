3ce77806793bb7da7be55b9f92a795b1c2c8c453
LemonGaming10 <65072711+LemonGaming10@users.noreply.github.com>
2020-05-31 15:49:07 +0530

    Update script.js

--- devspace/script.js
+++ devspace/script.js
@@ -1,166 +1,115 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-var ballRadius = 10;
-var x = canvas.width/2;
-var y = canvas.height-30;
-var dx = 2;
-var dy = -2;
-var paddleHeight = 10;
-var paddleWidth = 75;
-var paddleX = (canvas.width-paddleWidth)/2;
-var rightPressed = false;
-var leftPressed = false;
-var brickRowCount = 5;
-var brickColumnCount = 3;
-var brickWidth = 75;
-var brickHeight = 20;
-var brickPadding = 10;
-var brickOffsetTop = 30;
-var brickOffsetLeft = 30;
-var score = 0;
-var lives = 3;
+<!DOCTYPE HTML>
+<html> 
+    <head> 
+        <title>LemonGaming</title> 
+        <meta charset="utf-8"> 
+        <style> 
+           body {
+             margin: 0;
+             overflow: hidden;
+             }
+           .myCanvas {
+             position: absolute;
+             left:25%;
+             
+             }
+        </style>
+    </head> 
+    <body>
+        <canvas class="myCanvas">
+           <p>Add suitable fallback here.</p>
+        </canvas>
+        <script>
+           var canvas = document.querySelector('.myCanvas');
+           var height = canvas.height = window.innerHeight;
+           var width = canvas.width = height*0.75;
+           var ctx = canvas.getContext('2d');
+           var score = 0;
+           var x;
+           var cubecolors=[
+             'rgb(80, 80, 255)',
+             'rgb(153, 12, 235)',
+             'rgb(23, 227, 4)',
+             'rgb(242, 19, 138)'
+           ];
+           var cubes = [];
+           ctx.font = "1em serif";
+           function random(min, max) {
+              return Math.floor(Math.random() * (max - min) ) + min;
+           }
+           function dist(x1, x2, y1, y2) {
+              return Math.sqrt(Math.pow(x2-x1, 2)+Math.pow(y2-y1, 2));
+           }
+           function displayscore() {
+               score+=cubes[0].speed;
+               ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+               ctx.fillText('Score:', width/12, height/12);
+               ctx.fillText(Math.round(score), width/12, (height/12)+20);
+           }
+           function Cube(x, y, col) {
+              this.x=x;
+              this.y=y;
+              this.col=col;
+              this.size= 20;
+              this.speed= 3;
+           }
+           Cube.prototype.display = function() {
+              ctx.fillStyle = this.col;
+              ctx.fillRect(this.x, this.y, this.size, this.size);
+           };
+           Cube.prototype.update = function() {
+              this.y+=this.speed;
+              if(this.speed<6.5) {
+              this.speed+=0.001;
+              }
+           };
+           Cube.prototype.broadPhase = function() {
+              //return 
+           }
+           function Ball() {
+              this.x = width/2;
+              this.y = height*0.9;
+              this.radius = 12.5;
+              this.out = false;
+           }
+           Ball.prototype.display = function() {
+              ctx.beginPath();
+              ctx.arc(this.x, this.y, this.radius, 0, 2*Math.PI, true);
+              ctx.fillStyle ='rgba(255, 0, 0, 1)';
+              ctx.fill();
+              ctx.closePath();
+           };
+           var player = new Ball();
+           for(var i=0; i<25; i++) {
+              var col = Math.floor(Math.random()*4);
+              var x= Math.floor(random(0, (width-20)));
+              var y= Math.floor(random(-1250, 300));
+              cubes.push(new Cube(x, y, cubecolors[col]));
+           }
+           function updateBall(event) {
+              x = event.clientX - canvas.offsetLeft;
+             if(x > 12 && x < (canvas.width-12)) {
+                player.x = x;
+             }
+           }
+           canvas.addEventListener('mousemove', updateBall);
+           function draw() {
+              ctx.fillStyle = 'rgba(230, 230, 230, 1)';
+              ctx.fillRect(0, 0, width, height);
+              for(var i = 0; i<cubes.length; i++) {
+                 cubes[i].display();
+                 cubes[i].update();  
+                 if(cubes[i].y>height+cubes[0].size) {
+                    cubes[i].y = -1250;
+                    cubes[i].x = Math.floor(random(0, (width-20)));
+                 }
+              }
+              player.display();
+              displayscore();
+              requestAnimationFrame(draw);
+           }
+           draw();
+        </script>
 
-var bricks = [];
-for(var c=0; c<brickColumnCount; c++) {
-  bricks[c] = [];
-  for(var r=0; r<brickRowCount; r++) {
-    bricks[c][r] = { x: 0, y: 0, status: 1 };
-  }
-}
-
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
-document.addEventListener("mousemove", mouseMoveHandler, false);
-
-function keyDownHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = true;
-    }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = true;
-    }
-}
-
-function keyUpHandler(e) {
-    if(e.key == "Right" || e.key == "ArrowRight") {
-        rightPressed = false;
-    }
-    else if(e.key == "Left" || e.key == "ArrowLeft") {
-        leftPressed = false;
-    }
-}
-
-function mouseMoveHandler(e) {
-  var relativeX = e.clientX - canvas.offsetLeft;
-  if(relativeX > 0 && relativeX < canvas.width) {
-    paddleX = relativeX - paddleWidth/2;
-  }
-}
-function collisionDetection() {
-  for(var c=0; c<brickColumnCount; c++) {
-    for(var r=0; r<brickRowCount; r++) {
-      var b = bricks[c][r];
-      if(b.status == 1) {
-        if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
-          dy = -dy;
-          b.status = 0;
-          score++;
-          if(score == brickRowCount*brickColumnCount) {
-            alert("YOU WIN, CONGRATS!");
-            document.location.reload();
-          }
-        }
-      }
-    }
-  }
-}
-
-function drawBall() {
-  ctx.beginPath();
-  ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-  ctx.fillStyle = "#0095DD";
-  ctx.fill();
-  ctx.closePath();
-}
-function drawPaddle() {
-  ctx.beginPath();
-  ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-  ctx.fillStyle = "#0095DD";
-  ctx.fill();
-  ctx.closePath();
-}
-function drawBricks() {
-  for(var c=0; c<brickColumnCount; c++) {
-    for(var r=0; r<brickRowCount; r++) {
-      if(bricks[c][r].status == 1) {
-        var brickX = (r*(brickWidth+brickPadding))+brickOffsetLeft;
-        var brickY = (c*(brickHeight+brickPadding))+brickOffsetTop;
-        bricks[c][r].x = brickX;
-        bricks[c][r].y = brickY;
-        ctx.beginPath();
-        ctx.rect(brickX, brickY, brickWidth, brickHeight);
-        ctx.fillStyle = "#0095DD";
-        ctx.fill();
-        ctx.closePath();
-      }
-    }
-  }
-}
-function drawScore() {
-  ctx.font = "16px Arial";
-  ctx.fillStyle = "#0095DD";
-  ctx.fillText("Score: "+score, 8, 20);
-}
-function drawLives() {
-  ctx.font = "16px Arial";
-  ctx.fillStyle = "#0095DD";
-  ctx.fillText("Lives: "+lives, canvas.width-65, 20);
-}
-
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawBricks();
-  drawBall();
-  drawPaddle();
-  drawScore();
-  drawLives();
-  collisionDetection();
-
-  if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-    dx = -dx;
-  }
-  if(y + dy < ballRadius) {
-    dy = -dy;
-  }
-  else if(y + dy > canvas.height-ballRadius) {
-    if(x > paddleX && x < paddleX + paddleWidth) {
-      dy = -dy;
-    }
-    else {
-      lives--;
-      if(!lives) {
-       
-      }
-      else {
-        x = canvas.width/2;
-        y = canvas.height-30;
-        dx = 3;
-        dy = -3;
-        paddleX = (canvas.width-paddleWidth)/2;
-      }
-    }
-  }
-
-  if(rightPressed && paddleX < canvas.width-paddleWidth) {
-    paddleX += 7;
-  }
-  else if(leftPressed && paddleX > 0) {
-    paddleX -= 7;
-  }
-
-  x += dx;
-  y += dy;
-  requestAnimationFrame(draw);
-}
-
-draw()
+    </body>
+</html>
